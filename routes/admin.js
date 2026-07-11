f237a92a401ed3b4040cc94a333dc52b2855bc80
anonlevi <Solomonoke942@gmail.com@email.com>
2026-07-11 13:03:07 +0100

    Fix remaining case-sensitive model paths

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Report = require('../models/Report');
-const User = require('../models/User');
+const Report = require('../models/report');
+const User = require('../models/user');
 
 // Middleware to check if admin
 function isAdmin(req, res, next) {
